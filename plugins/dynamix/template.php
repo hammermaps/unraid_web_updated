718a21c5d42cbe27028b94b114a493e7a8fbaeca
Administrator <admin@example.com>
2021-05-25 22:59:37 +0200

    Update template.php

--- plugins/dynamix/template.php
+++ plugins/dynamix/template.php
@@ -17,6 +17,7 @@ require_once "$docroot/plugins/dynamix.core/common.php";
 require_once Common::getInstance()->getDocRoot()."/webGui/include/Helpers.php";
 
 // Get the webGui configuration preferences
+/** @var string $display */
 extract(parse_plugin_cfg('dynamix',true));
 
 // Read emhttp status
@@ -27,38 +28,32 @@ foreach (Common::getInstance()->getState()->getIndex() as $varname => $options) 
     unset($varname,$options);
 }
 
-/*
-$var     = (array)parse_ini_file('state/var.ini');
-$sec     = (array)parse_ini_file('state/sec.ini',true);
-$devs    = (array)parse_ini_file('state/devs.ini',true);
-$disks   = (array)parse_ini_file('state/disks.ini',true);
-$users   = (array)parse_ini_file('state/users.ini',true);
-$shares  = (array)parse_ini_file('state/shares.ini',true);
-$sec_nfs = (array)parse_ini_file('state/sec_nfs.ini',true);
-*/
-
 // Merge SMART settings
-require_once "$docroot/webGui/include/CustomMerge.php";
+require_once Common::getInstance()->getDocRoot()."/webGui/include/CustomMerge.php";
 
 // Pool devices
 $pool_devices = false;
 $pools = pools_filter($disks);
-foreach ($pools as $pool) $pool_devices |= $disks[$pool]['devices'];
+foreach ($pools as $pool) {
+    $pool_devices |= $disks[$pool]['devices'];
+}
 
 // Read network settings
-extract(parse_ini_file('state/network.ini',true));
+extract(Common::getInstance()->getState()->getState('network'));
 
 // Language translations
 $_SESSION['locale'] = $display['locale'];
 $_SESSION['buildDate'] = date('Ymd',$var['regBuildTime']);
-require_once "$docroot/webGui/include/Translations.php";
+
+require_once Common::getInstance()->getDocRoot()."/webGui/include/Translations.php";
 
 // Build webGui pages first, then plugins pages
-require_once "$docroot/webGui/include/PageBuilder.php";
+require_once Common::getInstance()->getDocRoot()."/webGui/include/PageBuilder.php";
 $site = [];
 build_pages('webGui/*.page');
 foreach (glob('plugins/*', GLOB_ONLYDIR) as $plugin) {
-  if ($plugin != 'plugins/dynamix') build_pages("$plugin/*.page");
+  if ($plugin != 'plugins/dynamix' && $plugin != 'plugins/dynamix.core')
+      build_pages("$plugin/*.page");
 }
 
 // Get general variables
@@ -71,7 +66,7 @@ $task = strtok($path,'/');
 
 // Here's the page we're rendering
 $myPage = $site[basename($path)];
-$pageroot = $docroot.'/'.dirname($myPage['file']);
+$pageroot = Common::getInstance()->getDocRoot().'/'.dirname($myPage['file']);
 
 // Giddyup
-require_once "$docroot/webGui/include/DefaultPageLayout.php";
+require_once Common::getInstance()->getDocRoot()."/webGui/include/DefaultPageLayout.php";
